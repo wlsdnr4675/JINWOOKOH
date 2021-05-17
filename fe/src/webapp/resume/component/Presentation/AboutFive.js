bbfebf6cb7604048a595fdf02e0b1b02a644174e
wlsdnr4675 <wlsdnr4675@gmail.com>
2021-05-17 20:24:45 +0900

    ERD FINALL 0517

---
+++ fe/src/webapp/resume/component/Presentation/AboutFive.js
@@ -0,0 +1,28 @@
+import React from "react";
+import parse from "html-react-parser";
+// Images
+import imgBg from "webapp/images/background/stars_bg_4.png";
+// Components
+import SkillbarSection from "webapp/resume/component/Presentation/SkillbarSection";
+
+const AboutFive = ({ data, title, backfont, text, children }) => (
+  <section
+    style={{ background: `url(${imgBg}) center center no-repeat #fff` }}
+    id="about"
+  >
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 col-sm-6 col-xs-12 xs-mb-50">
+          <h2 className="mt-0 font-700" data-backfont={backfont || "About"}>
+            {title && parse(title)}
+          </h2>
+          {text && parse(text)}
+        </div>
+        <SkillbarSection data={data} />
+      </div>
+      {children}
+    </div>
+  </section>
+);
+
+export default AboutFive;
---
+++ fe/src/webapp/resume/component/ResumeListFilter.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ResumeListFilter = ({ categories, classes }) => { 
+  console.log("fdsfds", classes)
+
+  return(
+  <div className="row mt-50">
+    <div className="col-md-12">
+      <div
+        id="portfolio-gallery-filter"
+        className={"cbp-l-filters-alignCenter text-sm-center " + classes}
+      >
+        <div
+          data-filter="*"
+          className="cbp-filter-item-active cbp-filter-item dark"
+        >
+          All
+        </div>
+        {categories.map((item, i) => (
+          <div
+            key={i}
+            data-filter={`.${item.replace(/\s+/g, "-").toLowerCase()}`}
+            className="cbp-filter-item"
+          >
+            {item.charAt(0).toUpperCase() + item.slice(1)}
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
+)};
+
+export default ResumeListFilter;
