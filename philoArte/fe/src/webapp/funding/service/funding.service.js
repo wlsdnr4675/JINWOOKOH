ef387165706cad0126a4ea1230013765ff3a2afd
wlsdnr4675 <wlsdnr4675@gmail.com>
2021-06-14 17:50:38 +0900

    token update 0613

--- philoArte/fe/src/webapp/funding/service/funding.service.js
+++ philoArte/fe/src/webapp/funding/service/funding.service.js
@@ -1,19 +1,47 @@
 import axios from 'axios'
 
+
+const userInfo = typeof window !== `undefined` ? JSON.parse(localStorage.getItem('artist')) : null;
+
 const getList=(page) =>{
-    return axios.get("/funding/list?page=" +page)
+    return axios({
+        url:"/funding/list?page=" +page,
+        method: 'get',
+        headers:{
+            Authorization: 'JWT fefege...'
+        }
+    })
 }
 const fundingRegister = (data) =>{
-    return axios.post("/funding/register", data)
+    return axios({
+        url: "/funding/register",
+        method: "post",
+        data: data,
+        headers:{
+            Authorization:  `Bearer ${userInfo.token}`
+        }
+    })
 }
 
 const showDetail =(fundingId)=>{
-    return axios.get(`/funding/${fundingId}`)
+    return axios({
+        url: `/funding/${fundingId}`,
+        method: 'get',
+        headers:{
+            Authorization: 'JWT fefege...'
+        }
+    })
 }
 
 const fundingUpdate = (fundingId,data) =>{
-    return axios.put(`/funding/edit/${fundingId}`,data)
-    .then(res =>{
+    return axios({
+        url: `/funding/edit/${fundingId}`,
+        method: 'put',
+        data: data,
+        headers:{
+            Authorization: 'JWT fefege...'
+        }
+    }).then(res =>{
         return res.data
     })
 }
@@ -22,19 +50,44 @@ const deleteFunding = (funding) =>{
     if(del){
     axios({
         url: `/funding/${funding}`,
-        method:'DELETE'
+        method:'DELETE',
+        headers:{
+            Authorization: 'JWT fefege...'
+        }
     }).then(res =>{
         alert("삭제 완료")
     }).catch(err =>{
         alert("삭제과정에서 에러 발생" +err)
     })}
 }
-const fileUpload = (formData) => axios.post(`/funding_file/upload_file`,formData,
-    {headers:{ "Content-Type": "multipart/form-data"}})
+const fileUpload = (formData) => {
+    return axios({
+        url: `/funding_file/upload_file`,
+        method: 'post',
+        data: formData,
+        headers:{ "Content-Type": "multipart/form-data",
+        Authorization: 'JWT fefege...'}
+    })
+}
 
-const deleteFile = (id) => axios.delete(`/funding_file/delete_file/${id}`)
+const deleteFile = (id) =>{
+    return axios({
+        url: `/funding_file/delete_file/${id}`,
+        method: 'delete',
+        headers:{
+            Authorization: 'JWT fefege...'
+        }
+    })
+}
 
 const searchSomething = (page,keyword)=>{
-    return axios.get("/funding/list/search",page,keyword)
+    return axios({
+        url: "/funding/list/search",
+        method: page,keyword,
+        headers:{
+            Authorization: 'JWT fefege...'
+        }
+    })
+    
 }
 export default {getList, fundingRegister,showDetail,deleteFunding,fundingUpdate,searchSomething,fileUpload,deleteFile}
