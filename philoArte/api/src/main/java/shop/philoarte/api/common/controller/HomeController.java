04bd650cb553cea812eea8856b96cfe62c595f90
wlsdnr4675 <wlsdnr4675@gmail.com>
2021-06-19 16:46:15 +0900

    building  0619

---
+++ philoArte/api/src/main/java/shop/philoarte/api/common/controller/HomeController.java
@@ -0,0 +1,18 @@
+package shop.philoarte.api.common.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return new SimpleDateFormat("yyyy / MM / dd / HH:mm:ss").format(new Date());
+    }
+}
